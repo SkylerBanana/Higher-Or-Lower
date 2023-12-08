015c8978f7517e87923db65660752482153e2fb2
SkylerBanana <Skyler_Banana@protonmail.com>
2023-12-08 18:49:35 -0500

    Added Play Button To Start The Game

--- AnimeHigherOrLower/src/App.jsx
+++ AnimeHigherOrLower/src/App.jsx
@@ -1,30 +1,19 @@
 import React, { useState, useEffect } from "react";
-import Game from "./Game";
+import ApiData from "./Components/ApiData";
 function App() {
-  const [anime, setAnime] = useState({});
-  function onComplete() {
-    const animeObj = JSON.parse(this.responseText);
-    setAnime(animeObj);
+  const [shown, setShown] = useState(false);
+  function Show() {
+    setShown(true);
   }
-
-  useEffect(() => {
-    const url =
-      "https://cors-anywhere.herokuapp.com/api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=100";
-    const request = new XMLHttpRequest();
-    request.addEventListener("load", onComplete);
-    request.open("GET", url);
-    request.setRequestHeader(
-      "X-MAL-CLIENT-ID",
-      "63f77e4caf7ff7334e9c7b343423fcf8"
+  if (shown === true) {
+    return (
+      <div>
+        <ApiData />
+      </div>
     );
-    request.send();
-  }, []);
-
-  return (
-    <div>
-      <Game passedCall={anime} />
-    </div>
-  );
+  } else {
+    <button onClick={Show()}>PLAY</button>;
+  }
 }
 
 export default App;
---
+++ AnimeHigherOrLower/src/Components/ApiData.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Game from "./Game";
+
+function ApiData() {
+  const [anime, setAnime] = useState({});
+
+  function onComplete() {
+    const animeObj = JSON.parse(this.responseText);
+    setAnime(animeObj);
+  }
+
+  useEffect(() => {
+    const url =
+      "https://cors-anywhere.herokuapp.com/api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=100";
+    const request = new XMLHttpRequest();
+    request.addEventListener("load", onComplete);
+    request.open("GET", url);
+    request.setRequestHeader(
+      "X-MAL-CLIENT-ID",
+      "63f77e4caf7ff7334e9c7b343423fcf8"
+    );
+    request.send();
+  }, []);
+
+  return (
+    <div>
+      <Game passedCall={anime} />
+    </div>
+  );
+}
+export default ApiData;
