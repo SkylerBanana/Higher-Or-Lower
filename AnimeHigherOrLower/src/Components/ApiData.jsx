7b7db3b50b0fe28fd133c0992d3b21a4d29c120f
SkylerBanana <Skyler_Banana@protonmail.com>
2023-12-15 03:26:45 -0500

    App No Longer Fetches Data Directly From Browser

--- AnimeHigherOrLower/src/Components/ApiData.jsx
+++ AnimeHigherOrLower/src/Components/ApiData.jsx
@@ -4,22 +4,20 @@ import Game from "./Game";
 function ApiData() {
   const [anime, setAnime] = useState({});
 
-  function onComplete() {
-    const animeObj = JSON.parse(this.responseText);
-    setAnime(animeObj);
-  }
-
   useEffect(() => {
-    const url =
-      "https://cors-anywhere.herokuapp.com/api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=100";
-    const request = new XMLHttpRequest();
-    request.addEventListener("load", onComplete);
-    request.open("GET", url);
-    request.setRequestHeader(
-      "X-MAL-CLIENT-ID",
-      "63f77e4caf7ff7334e9c7b343423fcf8"
-    );
-    request.send();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://node-js-server-8kvr.onrender.com/api/data"
+        );
+        const result = await response.json();
+        setAnime(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
